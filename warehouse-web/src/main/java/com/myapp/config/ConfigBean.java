878aebfa6cf36beb4c7c8736a3af2f7514a57805
MontolioV <vetal07_mail@mail.ru>
2018-03-23 18:06:46 +0200

    Trying to migrate to WildFly.

--- warehouse-web/src/main/java/com/myapp/config/ConfigBean.java
+++ warehouse-web/src/main/java/com/myapp/config/ConfigBean.java
@@ -5,6 +5,6 @@ import javax.faces.annotation.FacesConfig;
 /**
  * <p>Created by MontolioV on 15.03.18.
  */
-//@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
 public class ConfigBean {
 }

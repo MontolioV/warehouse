72f9331eda82548bd9945c9fc0d5ff7c2ee19470
MontolioV <vetal07_mail@mail.ru>
2018-03-29 18:12:09 +0300

    Missing class.

---
+++ warehouse-core/src/main/java/com/myapp/utils/TestSecurityConstants.java
@@ -0,0 +1,24 @@
+package com.myapp.utils;
+
+import com.myapp.security.Roles;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <p>Created by MontolioV on 29.03.18.
+ */
+public interface TestSecurityConstants {
+    String LOGIN_VALID = "LOGIN_VALID";
+    String LOGIN_INVALID = "LOGIN_INVALID";
+    String PASSWORD_VALID = "PASSWORD_VALID_and_reliable_123";
+    String PASSWORD_INVALID = "PASSWORD_INVALID";
+    String PASS_HASH_VALID = "PASS_HASH_VALID";
+    String PASS_HASH_INVALID = "PASS_HASH_INVALID";
+    String TOKEN_HASH_VALID = "TOKEN_HASH_VALID";
+    String TOKEN_HASH_INVALID = "TOKEN_HASH_INVALID";
+    List<Roles> ROLES_LIST = Collections.singletonList(Roles.USER);
+    Set<String> ROLES_STR_SET = Collections.singleton(Roles.USER.name());
+
+}
